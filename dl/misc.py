7d8c3b84186132e44b5c3f8ca45d4c76b718be17
Hzbeta <ihzbeta@outlook.com>
2022-12-15 01:59:10 +0800

    ✨ feat(misc): log现在可以不记录到文件

--- dl/misc.py
+++ dl/misc.py
@@ -1,22 +1,31 @@
 import logging, os, time
 
-def get_logger(name, log_dir,log_time, level=logging.DEBUG, fmt_str='[%(asctime)s][%(levelname).1s]%(message)s', date_fmt='%H:%M:%S'):
+
+def get_logger(name,
+               level=logging.DEBUG,
+               log_dir=None,
+               log_time=None,
+               fmt_str='[%(asctime)s][%(levelname).1s]%(message)s',
+               date_fmt='%H:%M:%S'):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     fmt = logging.Formatter(fmt_str, date_fmt)
     sh = logging.StreamHandler()
     sh.setFormatter(fmt)
     sh.setLevel(level)
-    os.makedirs(log_dir, exist_ok=True)
-    #日志文件名为当前时间
-    log_file_name = log_time + '.log'
-    log_file_path = os.path.join(log_dir, log_file_name)
-    fh = logging.FileHandler(log_file_path)
-    fh.setLevel(level)
-    fh.setFormatter(fmt)
     logger.addHandler(sh)
-    logger.addHandler(fh)
+    
+    if(log_dir and log_time):
+        os.makedirs(log_dir, exist_ok=True)
+        #日志文件名为当前时间
+        log_file_name = log_time + '.log'
+        log_file_path = os.path.join(log_dir, log_file_name)
+        fh = logging.FileHandler(log_file_path)
+        fh.setLevel(level)
+        fh.setFormatter(fmt)
+        logger.addHandler(fh)
     return logger
+
 
 def get_time():
     return time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime(time.time()))
