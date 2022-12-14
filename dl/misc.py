46b89d6c2bde0bfdaef5b9df2a1e1b835ef06788
Hzbeta <ihzbeta@outlook.com>
2022-12-14 22:19:10 +0800

    ✨ feat(misc): log现在同时记录到文件和控制台

--- dl/misc.py
+++ dl/misc.py
@@ -1,11 +1,19 @@
-import logging
+import logging, os, time
 
-def getLogger(name, level=logging.DEBUG, fmt_str='[%(asctime)s][%(levelname).1s]%(message)s', date_fmt='%H:%M:%S'):
+
+def getLogger(name, log_dir, level=logging.DEBUG, fmt_str='[%(asctime)s][%(levelname).1s]%(message)s', date_fmt='%H:%M:%S'):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     fmt = logging.Formatter(fmt_str, date_fmt)
     sh = logging.StreamHandler()
     sh.setFormatter(fmt)
     sh.setLevel(level)
+    #日志文件名为当前时间
+    log_file_name = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(time.time())) + '.log'
+    log_file_path = os.path.join(log_dir, log_file_name)
+    fh = logging.FileHandler(log_file_path)
+    fh.setLevel(level)
+    fh.setFormatter(fmt)
     logger.addHandler(sh)
+    logger.addHandler(fh)
     return logger
