13cdb664162d2ae7690f3bf0417fa3b695cc2287
Hzbeta <ihzbeta@outlook.com>
2023-03-18 19:03:25 +0800

    ✨ feat(media): add class Video
    use class Video get video info and frame more efficient

--- dl/media.py
+++ dl/media.py
@@ -1,14 +1,33 @@
 import cv2
 
-def get_frame(video_path,index):
+
+class Video():
+    def __init__(self, video_path):
+        self.video_path = video_path
+        self.cap = cv2.VideoCapture(video_path)
+        assert self.cap is not None, f'Cannot open video: {video_path}'
+        self.frame_nums = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.duration = self.frame_nums / self.fps
+
+    def get_frame(self, index):
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, index)  #设置要获取的帧号
+        ret, frame = self.cap.read()  #read方法返回一个布尔值和一个视频帧。若帧读取成功，则返回True
+        assert ret, f'can not read frame of {index} in {self.video_path}'
+        return frame
+
+
+def get_frame(video_path, index):
     cap = cv2.VideoCapture(video_path)  #返回一个capture对象
     cap.set(cv2.CAP_PROP_POS_FRAMES, index)  #设置要获取的帧号
     ret, frame = cap.read()  #read方法返回一个布尔值和一个视频帧。若帧读取成功，则返回True
-    assert ret,f'无法读取视频{video_path}的第{index}帧'
+    assert ret, f'无法读取视频{video_path}的第{index}帧'
     return frame
 
 
-def resize_imgs(base_img, other_imgs:list, method=cv2.INTER_AREA):
+def resize_imgs(base_img, other_imgs: list, method=cv2.INTER_AREA):
     """缩放图像列表至基准图像大小
 
     Args:
@@ -19,12 +38,15 @@ def resize_imgs(base_img, other_imgs:list, method=cv2.INTER_AREA):
     Returns:
         img_list: 处理后的图像列表，顺序不变
     """
-    for i,img in enumerate(other_imgs):
-        if not img.shape==base_img.shape:
-            other_imgs[i] = cv2.resize(img, (base_img.shape[1], base_img.shape[0]), interpolation=method)
+    for i, img in enumerate(other_imgs):
+        if not img.shape == base_img.shape:
+            other_imgs[i] = cv2.resize(img,
+                                       (base_img.shape[1], base_img.shape[0]),
+                                       interpolation=method)
     return other_imgs
 
-def get_imgs_diff_score(base_img,other_imgs:list):
+
+def get_imgs_diff_score(base_img, other_imgs: list):
     """获取图像列表和基准图片的相似度
 
     Args:
@@ -35,7 +57,7 @@ def get_imgs_diff_score(base_img,other_imgs:list):
         similarity_list : 按顺序的图像相似度list
     """
     other_imgs = resize_imgs(base_img, other_imgs)
-    similarity_list=[]
+    similarity_list = []
     for img in other_imgs:
         errorL2 = cv2.norm(base_img, img, cv2.NORM_L2)
         similarity = 1 - errorL2 / (base_img.shape[0] * base_img.shape[1])
